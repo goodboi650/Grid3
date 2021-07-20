9d86c32b00c8b51820b8ffda0650967428b7af58
goodboi650 <utkarshlal650@gmail.com>
2021-07-21 03:29:31 +0530

    added basic html and css, placeholder text needs to be changed later

--- Grid/gridapp/urls.py
+++ Grid/gridapp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from gridapp import views
 
 urlpatterns = [
@@ -9,5 +8,10 @@ urlpatterns = [
          name='submit_one_request'),
     path('create_response/', views.CreateResponse.as_view(), name='create_response'),
     path('delete/', views.DeleteResponse.as_view(), name='delete'),
-    path('search/', views.SearchResponse.as_view(), name='search')
+    path('search/', views.SearchResponse.as_view(), name='search'),
+
+    path('', views.user, name='user'),
+    path('login/', views.login, name='login'),
+    path('gridadmin/', views.gridadmin, name='admin'),
+    path('ipscreen/', views.ipscreen, name='ipscreen'),
 ]
