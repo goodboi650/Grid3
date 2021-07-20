eef8de0004728baa172cb25557ef2a8d2b88d029
Sourabh S Yelluru <42965936+SourabhYelluru131@users.noreply.github.com>
2021-07-20 15:06:27 +0530

    ..

---
+++ Grid/gridapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Response(models.Model):
+    AssetName = models.CharField(max_length=100, primary_key=True)
+    ##
+    Password = models.CharField(max_length=50) #Secret
+    Username = models.CharField(max_length=50) #Secret
+    Server = models.CharField(max_length=20)
+    Port = models.IntegerField(blank=True, null=True)
+    ##
+    IP = models.CharField(max_length=100)
+    Hostname = models.CharField(max_length=100)
+    MAC = models.CharField(max_length=100)
+    OS = models.CharField(max_length=100)
+    #Domaininfo
+    Status = models.BooleanField
+    LastUpdated = models.TimeField
---
+++ Grid/gridapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from gridapp import views
+
+urlpatterns = [
+    path('onerequest', views.SubmitOneRequest.as_view(), name='submit_one_request'),
+    path('allrequest', views.SubmitAllRequest.as_view(), name='submit_one_request'),
+    path('create_response', views.CreateResponse.as_view(), name='create_response')
+]
