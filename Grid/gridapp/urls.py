003fce92888a9bf1d5ca1896a42e79fcf7634f27
Sourabh S Yelluru <42965936+SourabhYelluru131@users.noreply.github.com>
2021-07-21 17:02:06 +0530

    T-T

--- Grid/gridapp/urls.py
+++ Grid/gridapp/urls.py
@@ -2,16 +2,9 @@ from django.urls import path
 from gridapp import views
 
 urlpatterns = [
-    path('onerequest/', views.SubmitOneRequest.as_view(),
-         name='submit_one_request'),
-    path('allrequest/', views.SubmitAllRequest.as_view(),
-         name='submit_one_request'),
-    path('create_response/', views.CreateResponse.as_view(), name='create_response'),
-    path('delete/', views.DeleteResponse.as_view(), name='delete'),
-    path('search/', views.SearchResponse.as_view(), name='search'),
-
-    path('', views.user, name='user'),
-    path('login/', views.login, name='login'),
-    path('gridadmin/', views.gridadmin, name='admin'),
-    path('ipscreen/', views.ipscreen, name='ipscreen'),
+    path('onerequest', views.SubmitOneRequest.as_view(), name='submit_one_request'),
+    path('allrequest', views.SubmitAllRequest.as_view(), name='submit_all_request'),
+    path('create_response', views.CreateResponse.as_view(), name='create_response'),
+    path('delete', views.DeleteResponse.as_view(), name='delete'),
+    path('search', views.SearchResponse.as_view(), name='search')
 ]
