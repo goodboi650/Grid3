f80f1478f27f81b439d8c0c1ee338c29e79ed2f9
goodboi650 <utkarshlal650@gmail.com>
2021-07-21 21:46:15 +0530

    implemented basic login, site can be broken

--- Grid/gridapp/urls.py
+++ Grid/gridapp/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from gridapp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('onerequest', views.SubmitOneRequest.as_view(), name='submit_one_request'),
     path('allrequest', views.SubmitAllRequest.as_view(), name='submit_all_request'),
     path('create_response', views.CreateResponse.as_view(), name='create_response'),
     path('delete', views.DeleteResponse.as_view(), name='delete'),
-    path('search', views.SearchResponse.as_view(), name='search')
+    path('search', views.SearchResponse.as_view(), name='search'),
+
+    path('', views.user, name='user'),
+    path('gridadmin', views.gridadmin, name='grid-admin'),
+    path('login', auth_views.LoginView.as_view(
+        template_name='gridapp/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(
+        template_name='gridapp/logout.html'), name='logout'),
+    path('ipscreen', views.ipscreen, name='ip-screen'),
 ]
